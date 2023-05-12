0e5e67718c875028c5368ffa3645a7a9c75ccd75
504BassSlapper <504bassslapper@gmail.com>
2023-05-12 20:24:54 +0100

    *check chain validity * refactor + add comments

--- src/main/java/org/sec/crypto/BasicChain.java
+++ src/main/java/org/sec/crypto/BasicChain.java
@@ -6,27 +6,29 @@ import org.sec.crypto.models.Block;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class BasicChain {
-    public static final int CHAIN_COMPLEXITY = 5;
+    // example for casual machine processing (BTC chain up to ~ 48)
+    public static final int NETWORK_DIFFICULTY = 5;
     private static List<Block> basicBlockChain = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         Block firstBlock = new Block("0", "First block");
-        firstBlock.minedBlock(CHAIN_COMPLEXITY);
+        firstBlock.minedBlock(NETWORK_DIFFICULTY);
 
         Block secondBlock = new Block(firstBlock.getHash(), "Second block");
-        secondBlock.minedBlock(CHAIN_COMPLEXITY);
+        secondBlock.minedBlock(NETWORK_DIFFICULTY);
 
         Block thirdBlock = new Block(secondBlock.getHash(), "Third Block");
-        thirdBlock.minedBlock(CHAIN_COMPLEXITY);
-
-        System.out.println(firstBlock.getData() + " mined : " + firstBlock.getHash());
-        System.out.println(secondBlock.getData() + " mined : " + secondBlock.getHash());
-        System.out.println(thirdBlock.getData() + " minde : " + thirdBlock.getHash());
+        thirdBlock.minedBlock(NETWORK_DIFFICULTY);
 
         basicBlockChain = List.of(firstBlock, secondBlock, thirdBlock);
+        if(isChainValid()){
+            System.out.println("Chain is valid");
+        }
+        else {
+            System.out.println("Chain is not valid");
+        }
         ObjectMapper om = new ObjectMapper();
         String basicBlockChainJson = om.writerWithDefaultPrettyPrinter().writeValueAsString(basicBlockChain);
         System.out.println("Block chain : ");
@@ -34,10 +36,10 @@ public class BasicChain {
 
     }
 
-    public Boolean isChainValid() {
+    public static boolean isChainValid() {
         Block currentBlock;
         Block previousBlock;
-        String hashTarget = new String(new char[CHAIN_COMPLEXITY]).replace('\0', '0');
+        String hashTarget = new String(new char[NETWORK_DIFFICULTY]).replace('\0', '0');
 
         for (int i = 1; i < basicBlockChain.size(); i++) {
             currentBlock = basicBlockChain.get(i);
@@ -49,11 +51,11 @@ public class BasicChain {
             if (!previousBlock.getHash().equals(currentBlock.getPreviousHash())) {
                 return false;
             }
-            if (!currentBlock.getHash().substring(0, CHAIN_COMPLEXITY).equals(hashTarget)) {
+            if (!currentBlock.getHash().substring(0, NETWORK_DIFFICULTY).equals(hashTarget)) {
                 System.out.println("the block : " + currentBlock.getData() + " has not been mined yet");
                 return false;
             }
         }
-        return Boolean.TRUE;
+        return true;
     }
 }
--- src/main/java/org/sec/crypto/models/Block.java
+++ src/main/java/org/sec/crypto/models/Block.java
@@ -1,16 +1,16 @@
 package org.sec.crypto.models;
 
-import lombok.Data;
+import lombok.Getter;
 import org.sec.crypto.utils.StringUtil;
 
 import java.util.Date;
 
-@Data
+@Getter
 public class Block {
     private String hash;
-    private String previousHash;
-    private String data;
-    private long timestamp;
+    private final String previousHash;
+    private final String data;
+    private final long timestamp;
     private int nonce;
 
     /**
@@ -35,26 +35,26 @@ public class Block {
      */
     public String calculateHash() {
         return StringUtil.applySha256(this.previousHash
-                + timestamp+ nonce
+                + timestamp + nonce
                 + data);
 
     }
 
     /**
-     * new char[complexity] creates a new char array of length complexity.
+     * new char[networkDifficulty] creates a new char array of length networkDifficulty.
      * new String(...) creates a new String object using the contents of the char array as the initial value.
      * replace("\0","0") replaces all occurrences of the null character (\0) in the string with the digit 0.
-     *
+     * <p>
      * Since the newly created char array is filled with null characters,
      * the replace method replaces all of them with the digit 0,
-     * resulting in a string consisting of complexity number of zeros.
+     * resulting in a string consisting of networkDifficulty number of zeros.
      *
-     * @param complexity Integer
+     * @param networkDifficulty Integer
      */
-    public void minedBlock(int complexity) {
-        String target = new String(new char[complexity]).replace('\0', '0'); //Create a string with difficulty * "0"
-        while(!hash.substring( 0, complexity).equals(target)) {
-            nonce ++;
+    public void minedBlock(int networkDifficulty) {
+        String target = new String(new char[networkDifficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while (!hash.substring(0, networkDifficulty).equals(target)) {
+            nonce++;
             hash = calculateHash();
         }
         System.out.println("Block Mined  : " + hash);
